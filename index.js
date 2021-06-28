0b13d760b4aecaf3aaf9e5a19e3f8491bededf2f
TenzingS <thesherpatenzing@gmail.com>
2021-06-28 16:29:42 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if(distance < 42){
+        return (42 - distance);
+    }else{
+        return (distance - 42);
+    }
+}
+
+distanceFromHqInBlocks(43);
+
+
+function distanceFromHqInFeet(distance){
+    return (distanceFromHqInBlocks(distance) * 264);
+}
+
+function distanceTravelledInFeet(start, end){
+    if (start >= end){
+        return ((start - end) * 264);
+    }
+    else {
+        return ((end - start) * 264 )
+    };
+}
+
+function calculatesFarePrice(start, destination){
+    const distanceFeet = distanceTravelledInFeet(start, destination)
+
+    if(distanceFeet <= 400){
+        return 0 
+    }else if(distanceFeet > 400 && distanceFeet <= 2000){
+       const distancetravelled = distanceFeet - 400 
+       return distancetravelled * .02
+    }
+    else if(distanceFeet > 2000 && distanceFeet <= 2500){
+        return 25
+    }
+    else{
+        return 'cannot travel that far'
+    }
+}
