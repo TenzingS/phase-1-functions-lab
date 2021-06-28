1ca462a8465b90516771938033efe20f11a98d0c
TenzingS <thesherpatenzing@gmail.com>
2021-06-28 16:27:40 -0400

    commit tree fc7036cbc8aa1ef8cbe5e2efd2982d56e011062c

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if(distance < 42){
+        return (42 - distance);
+    }else{
+        return (distance - 42);
+    }
+}
+
+distanceFromHqInBlocks(43);
+
+
+function distanceFromHqInFeet(distance){
+    return (distanceFromHqInBlocks(distance) * 264);
+}
+
